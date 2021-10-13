699c931a0477b05ae50b0c55a3e8d8de0a145e15
tennessee_wallaceh <tennessee.wallaceh@gmail.com>
2021-10-13 14:22:20 +0100

    Add match query functionality

--- R/neo4j_query.R
+++ R/neo4j_query.R
@@ -1,3 +1,5 @@
+source('./R/match_queries.R') # Provides options for expanding match rows
+
 footr_setup = function() {
   ip = Sys.getenv("FOOTR_NEO_IP")
   if (ip == "") {
@@ -15,7 +17,7 @@ footr_setup = function() {
     ip=ip, 
     password=password, 
     username=username, 
-    max_active_queries=700 # default is 1000, so give some slack
+    max_active_queries=800 # default is 1000, so give some slack
   )
 
   return(config)
@@ -96,38 +98,6 @@ neo_query <- function(cypher_query, verbose=FALSE) {
   }
 }
 
-get_home_team = function(match_id) {
-  cypher_query = glue::glue('MATCH (match:Match {{uid: \'{match_id}\'})-[:HOME_TEAM]-(home_team:Team) RETURN home_team')
-  raw = execute_cypher(cypher_query)
-  return(raw[[1]]$row[[1]][[2]])
-}
-
-get_away_team = function(match_id) {
-  cypher_query = glue::glue('MATCH (match:Match {{uid: \'{match_id}\'})-[:AWAY_TEAM]-(away_team:Team) RETURN away_team')
-  raw = execute_cypher(cypher_query)
-  return(raw[[1]]$row[[1]][[2]])
-}
-
-#' Get the home goals for a particular match, selected by match id
-#'
-#' @param match_id 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-get_home_goals = function(match_id) {
-  cypher_query = glue::glue('MATCH (:Match {{uid: \'{match_id}\'})-[goal:GOAL]-(:Goal) WHERE goal.team_loc = \'H\' RETURN count(*)')
-  raw = execute_cypher(cypher_query)
-  return(raw[[1]]$row)
-}
-
-get_away_goals = function(match_id) {
-  query_string = glue::glue('MATCH (:Match {{uid: \'{match_id}\'})-[goal:GOAL]-(:Goal) WHERE goal.team_loc = \'A\' RETURN count(*)')
-  raw = execute_cypher(query_string)
-  return(raw[[1]]$row)
-}
-
 #' Get matches from the DB
 #'
 #' @param max_n The maximum number of games to return, for now limit set to NULL
@@ -137,7 +107,7 @@ get_away_goals = function(match_id) {
 #'
 #' @examples
 #' get_matches(max_n=NULL) # get all matches
-get_matches = function(max_n=10) {
+get_matches = function(expansions, max_n=10) {
   cypher_query = 'MATCH (match_node:Match) RETURN match_node'
 
   if (!is.null(max_n)) {
@@ -147,31 +117,34 @@ get_matches = function(max_n=10) {
   raw_data = execute_cypher(cypher_query,  verbose=TRUE)
 
   n_matches = length(raw_data)
-
-  date = vector(mode="character", length=n_matches)
-  home_team = vector(mode="character", length=n_matches)
-  away_team = vector(mode="character", length=n_matches)
-  home_goals = vector(mode="integer", length=n_matches)
-  away_goals = vector(mode="integer", length=n_matches)
-  result = vector(mode="character", length=n_matches)
+  
+  expanded_data = data.frame(
+    'date'=vector(mode="character", length=n_matches),
+    'result'=vector(mode="character", length=n_matches),
+    stringsAsFactors=FALSE
+  )
+  
+  for (expansion in expansions) {
+    if(!(expansion %in% names(match_queries))) {
+      stop(glue::glue('Match expansion {expansion} is not defined! Check match_queries.R'))
+    }
+    expanded_data[expansion] = vector(match_queries[[c(expansion, 'type')]], length=n_matches)
+  }
   
   progress_bar = txtProgressBar(max=n_matches)
   print(glue::glue('Found {n_matches} matches, building match data...'))
   for (match in 1:n_matches) {
     match_data = raw_data[[match]][[1]][[1]]
-    
     match_id = match_data['uid']
-    date[match] = match_data['date']
 
-    home_team[match] = get_home_team(match_id)
-    home_goals[match] = get_home_goals(match_id)
-
-    away_team[match] = get_away_team(match_id)
-    away_goals[match] = get_away_goals(match_id)
-    result[match] = match_data['result']
+    expanded_data[['date']][match] = match_data['date'][[1]]
+    expanded_data[['result']][match] = match_data['result'][[1]]
     
+    for (expansion in expansions) {
+      expanded_data[[expansion]][match] = match_queries[[c(expansion, 'fcn')]](match_id)
+    }
+
     setTxtProgressBar(progress_bar, match)
   }
-
-  return(data.frame(date, home_team, home_goals, away_team, away_goals, result))
+  return(expanded_data)
 }
